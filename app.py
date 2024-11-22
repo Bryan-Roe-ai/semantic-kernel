4e72a8cb04b878d667071aa0f4c688d0da7a021b
Bryan <74067792+Bryan-Roe@users.noreply.github.com>
2024-11-22 13:49:25 +0000

    Update VSCode and project configurations to enhance development environment and add FastAPI support

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def greet_json():
     return {"Hello": "World!"}
+
+{
+  "name": "Python: FastAPI",
+  "type": "debugpy",
+  "request": "launch",
+  "program": "${workspaceFolder}/app.py",
+  "args": ["run", "--host", "0.0.0.0", "--port", "8000"],
+  "console": "integratedTerminal",
+  "envFile": "${workspaceFolder}/.env.local.template"
+}
