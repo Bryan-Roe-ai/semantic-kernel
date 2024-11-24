938ba9ad7ae746637d248103eb1cedd79823eafc
Bryan <74067792+Bryan-Roe@users.noreply.github.com>
2024-11-24 10:29:25 +0000

    Set up FastAPI application; update launch configuration and VSCode settings

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def greet_json():
     return {"Hello": "World!"}
-
-{
-  "name": "Python: FastAPI",
-  "type": "debugpy",
-  "request": "launch",
-  "program": "${workspaceFolder}/app.py",
-  "args": ["run", "--host", "0.0.0.0", "--port", "8000"],
-  "console": "integratedTerminal",
-  "envFile": "${workspaceFolder}/.env.local.template"
-}
