23f700e70314095ea530a37aaf05b9a25f12a103
Bryan Roe <74067792+Bryan-Roe@users.noreply.github.com>
2025-06-21 13:54:15 -0700

    Bump coverlet.collector from 6.0.0 to 6.0.1 in /dotnet
    Bumps [coverlet.collector](https://github.com/coverlet-coverage/coverlet) from 6.0.0 to 6.0.1.
    - [Release notes](https://github.com/coverlet-coverage/coverlet/releases)
    - [Commits](https://github.com/coverlet-coverage/coverlet/compare/v6.0.0...v6.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: coverlet.collector
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- 01-core-implementations/dotnet/src/SemanticKernel.UnitTests/Functions/KernelFunctionFromMethodTests2.cs
+++ 01-core-implementations/dotnet/src/SemanticKernel.UnitTests/Functions/KernelFunctionFromMethodTests2.cs
@@ -228,20 +228,19 @@ public sealed class KernelFunctionFromMethodTests2
     }
 
     private sealed class InvalidPlugin
-    public async Task ItFlowsSpecialArgumentsIntoFunctionsAsync()
     {
         KernelBuilder builder = new();
         builder.Services.AddLogging(c => c.SetMinimumLevel(LogLevel.Warning));
         Kernel kernel = builder.Build();
         kernel.Culture = new CultureInfo("fr-FR");
         KernelArguments args = [];
-        using CancellationTokenSource cts = new();
+        using CancellationTokenSource cts = new ();
 
         bool invoked = false;
-        KernelFunction func = null!;
-        func = KernelFunctionFactory.CreateFromMethod(
-            (Kernel kernelArg, KernelFunction funcArg, KernelArguments argsArg, ILoggerFactory loggerFactoryArg,
-             ILogger loggerArg, IAIServiceSelector serviceSelectorArg, CultureInfo cultureArg, CancellationToken cancellationToken) =>
+KernelFunction func = null!;
+func = KernelFunctionFactory.CreateFromMethod(
+    (Kernel kernelArg, KernelFunction funcArg, KernelArguments argsArg, ILoggerFactory loggerFactoryArg,
+     ILogger loggerArg, IAIServiceSelector serviceSelectorArg, CultureInfo cultureArg, CancellationToken cancellationToken) =>
             {
                 Assert.Same(kernel, kernelArg);
                 Assert.Same(func, funcArg);
@@ -254,318 +253,318 @@ public sealed class KernelFunctionFromMethodTests2
                 invoked = true;
             });
 
-        await func.InvokeAsync(kernel, args, cts.Token);
+await func.InvokeAsync(kernel, args, cts.Token);
 
-        Assert.True(invoked);
+Assert.True(invoked);
     }
 
     [Fact]
-    public async Task ItInjectsServicesFromDIIntoFunctionsAsync()
-    {
-        var serviceA = new ExampleService();
-        var serviceB = new ExampleService();
-        var serviceC = new ExampleService();
+public async Task ItInjectsServicesFromDIIntoFunctionsAsync()
+{
+    var serviceA = new ExampleService();
+    var serviceB = new ExampleService();
+    var serviceC = new ExampleService();
 
-        KernelBuilder builder = new();
-        builder.Services.AddKeyedSingleton<IExampleService>("something", serviceA);
-        builder.Services.AddSingleton<IExampleService>(serviceB);
-        builder.Services.AddKeyedSingleton<IExampleService>("somethingelse", serviceC);
-        Kernel kernel = builder.Build();
+    KernelBuilder builder = new();
+    builder.Services.AddKeyedSingleton<IExampleService>("something", serviceA);
+    builder.Services.AddSingleton<IExampleService>(serviceB);
+    builder.Services.AddKeyedSingleton<IExampleService>("somethingelse", serviceC);
+    Kernel kernel = builder.Build();
 
-        bool invoked = false;
-        KernelFunction func = KernelFunctionFactory.CreateFromMethod(
-            ([FromKernelServices] IExampleService service1Arg,
-             [FromKernelServices("something")] IExampleService service2Arg,
-             [FromKernelServices("somethingelse")] IExampleService service3Arg,
-             [FromKernelServices] IExampleService service4Arg,
-             [FromKernelServices("doesntexist")] IExampleService? service5Arg = null) =>
-            {
-                Assert.Same(serviceB, service1Arg);
-                Assert.Same(serviceA, service2Arg);
-                Assert.Same(serviceC, service3Arg);
-                Assert.Same(serviceB, service4Arg);
-                Assert.Null(service5Arg);
-                invoked = true;
-            });
-
-        await func.InvokeAsync(kernel);
-
-        Assert.True(invoked);
-
-        Assert.DoesNotContain(func.Metadata.Parameters, p => p.Name.Contains("service", StringComparison.Ordinal));
-    }
-
-    [Fact]
-    public async Task ItThrowsForMissingServicesWithoutDefaultsAsync()
-    {
-        Kernel kernel = new();
-        KernelFunction func;
-
-        func = KernelFunctionFactory.CreateFromMethod(([FromKernelServices] IExampleService service) => { });
-        await Assert.ThrowsAsync<KernelException>(() => func.InvokeAsync(kernel));
-
-        func = KernelFunctionFactory.CreateFromMethod(([FromKernelServices] IExampleService? service) => { });
-        await Assert.ThrowsAsync<KernelException>(() => func.InvokeAsync(kernel));
-
-        func = KernelFunctionFactory.CreateFromMethod(([FromKernelServices("name")] IExampleService? service) => { });
-        await Assert.ThrowsAsync<KernelException>(() => func.InvokeAsync(kernel));
-    }
-
-    [Fact]
-    public void ItMakesProvidedExtensionPropertiesAvailableViaMetadataWhenConstructedFromDelegate()
-    {
-        // Act.
-        var func = KernelFunctionFactory.CreateFromMethod(() => { return "Value1"; }, new KernelFunctionFromMethodOptions
+    bool invoked = false;
+    KernelFunction func = KernelFunctionFactory.CreateFromMethod(
+        ([FromKernelServices] IExampleService service1Arg,
+         [FromKernelServices("something")] IExampleService service2Arg,
+         [FromKernelServices("somethingelse")] IExampleService service3Arg,
+         [FromKernelServices] IExampleService service4Arg,
+         [FromKernelServices("doesntexist")] IExampleService? service5Arg = null) =>
         {
-            AdditionalMetadata = new ReadOnlyDictionary<string, object?>(new Dictionary<string, object?>
-            {
-                ["key1"] = "value1",
-            })
+            Assert.Same(serviceB, service1Arg);
+            Assert.Same(serviceA, service2Arg);
+            Assert.Same(serviceC, service3Arg);
+            Assert.Same(serviceB, service4Arg);
+            Assert.Null(service5Arg);
+            invoked = true;
         });
 
-        // Assert.
-        Assert.Contains("key1", func.Metadata.AdditionalProperties.Keys);
-        Assert.Equal("value1", func.Metadata.AdditionalProperties["key1"]);
-    }
+    await func.InvokeAsync(kernel);
 
-    [Fact]
-    public void ItMakesProvidedExtensionPropertiesAvailableViaMetadataWhenConstructedFromMethodInfo()
+    Assert.True(invoked);
+
+    Assert.DoesNotContain(func.Metadata.Parameters, p => p.Name.Contains("service", StringComparison.Ordinal));
+}
+
+[Fact]
+public async Task ItThrowsForMissingServicesWithoutDefaultsAsync()
+{
+    Kernel kernel = new();
+    KernelFunction func;
+
+    func = KernelFunctionFactory.CreateFromMethod(([FromKernelServices] IExampleService service) => { });
+    await Assert.ThrowsAsync<KernelException>(() => func.InvokeAsync(kernel));
+
+    func = KernelFunctionFactory.CreateFromMethod(([FromKernelServices] IExampleService? service) => { });
+    await Assert.ThrowsAsync<KernelException>(() => func.InvokeAsync(kernel));
+
+    func = KernelFunctionFactory.CreateFromMethod(([FromKernelServices("name")] IExampleService? service) => { });
+    await Assert.ThrowsAsync<KernelException>(() => func.InvokeAsync(kernel));
+}
+
+[Fact]
+public void ItMakesProvidedExtensionPropertiesAvailableViaMetadataWhenConstructedFromDelegate()
+{
+    // Act.
+    var func = KernelFunctionFactory.CreateFromMethod(() => { return "Value1"; }, new KernelFunctionFromMethodOptions
     {
-        // Arrange.
-        var target = new LocalExamplePlugin();
-        var methodInfo = target.GetType().GetMethod(nameof(LocalExamplePlugin.Type02))!;
-
-        // Act.
-        var func = KernelFunctionFactory.CreateFromMethod(methodInfo, target, new KernelFunctionFromMethodOptions
+        AdditionalMetadata = new ReadOnlyDictionary<string, object?>(new Dictionary<string, object?>
         {
-            AdditionalMetadata = new ReadOnlyDictionary<string, object?>(new Dictionary<string, object?>
-            {
-                ["key1"] = "value1",
-            })
-        });
+            ["key1"] = "value1",
+        })
+    });
 
-        // Assert.
-        Assert.Contains("key1", func.Metadata.AdditionalProperties.Keys);
-        Assert.Equal("value1", func.Metadata.AdditionalProperties["key1"]);
-    }
+    // Assert.
+    Assert.Contains("key1", func.Metadata.AdditionalProperties.Keys);
+    Assert.Equal("value1", func.Metadata.AdditionalProperties["key1"]);
+}
 
-    [Fact]
-    public void ItShouldExposeUnderlyingMethod()
+[Fact]
+public void ItMakesProvidedExtensionPropertiesAvailableViaMetadataWhenConstructedFromMethodInfo()
+{
+    // Arrange.
+    var target = new LocalExamplePlugin();
+    var methodInfo = target.GetType().GetMethod(nameof(LocalExamplePlugin.Type02))!;
+
+    // Act.
+    var func = KernelFunctionFactory.CreateFromMethod(methodInfo, target, new KernelFunctionFromMethodOptions
     {
-        // Arrange
-        var target = new LocalExamplePlugin();
-
-        var methodInfo = target.GetType().GetMethod(nameof(LocalExamplePlugin.FunctionWithCustomAttribute))!;
-
-        var kernelFunction = KernelFunctionFactory.CreateFromMethod(methodInfo, target);
-
-        // Assert
-        Assert.NotNull(kernelFunction.UnderlyingMethod);
-
-        Assert.Equal(methodInfo, kernelFunction.UnderlyingMethod);
-
-        Assert.NotNull(kernelFunction.UnderlyingMethod.GetCustomAttribute<CustomAttribute>());
-    }
-
-    private interface IExampleService;
-
-    private sealed class ExampleService : IExampleService;
-
-    private sealed class LocalExamplePlugin
-    {
-        [KernelFunction]
-        public void Type01()
+        AdditionalMetadata = new ReadOnlyDictionary<string, object?>(new Dictionary<string, object?>
         {
-        }
+            ["key1"] = "value1",
+        })
+    });
 
-        [KernelFunction]
-        public string Type02()
-        {
-            return "";
-        }
+    // Assert.
+    Assert.Contains("key1", func.Metadata.AdditionalProperties.Keys);
+    Assert.Equal("value1", func.Metadata.AdditionalProperties["key1"]);
+}
 
-        [KernelFunction]
-        public string? Type02Nullable()
-        {
-            return null;
-        }
+[Fact]
+public void ItShouldExposeUnderlyingMethod()
+{
+    // Arrange
+    var target = new LocalExamplePlugin();
 
-        [KernelFunction]
-        public async Task<string> Type03Async()
-        {
-            await Task.Delay(0);
-            return "";
-        }
+    var methodInfo = target.GetType().GetMethod(nameof(LocalExamplePlugin.FunctionWithCustomAttribute))!;
 
-        [KernelFunction]
-        public async Task<string?> Type03NullableAsync()
-        {
-            await Task.Delay(0);
-            return null;
-        }
+    var kernelFunction = KernelFunctionFactory.CreateFromMethod(methodInfo, target);
 
-        [KernelFunction]
-        public void Type04(string input)
-        {
-        }
+    // Assert
+    Assert.NotNull(kernelFunction.UnderlyingMethod);
 
-        [KernelFunction]
-        public void Type04Nullable(string? input)
-        {
-        }
+    Assert.Equal(methodInfo, kernelFunction.UnderlyingMethod);
 
-        [KernelFunction]
-        public void Type04Optional([Optional] string input)
-        {
-        }
+    Assert.NotNull(kernelFunction.UnderlyingMethod.GetCustomAttribute<CustomAttribute>());
+}
 
-        [KernelFunction]
-        public string Type05(string input)
-        {
-            return "";
-        }
+private interface IExampleService;
 
-        [KernelFunction]
-        private string? Type05Nullable(string? input = null)
-        {
-            return "";
-        }
+private sealed class ExampleService : IExampleService;
 
-        [KernelFunction]
-        internal string? Type05EmptyDefault(string? input = "")
-        {
-            return "";
-        }
-
-        [KernelFunction]
-        public string? Type05DefaultProvided(string? input = "someDefault")
-        {
-            return "";
-        }
-
-        [KernelFunction]
-        public async Task<string> Type06Async(string input)
-        {
-            await Task.Delay(0);
-            return "";
-        }
-
-        [KernelFunction]
-        public async Task<string?> Type06NullableAsync(string? input)
-        {
-            await Task.Delay(0);
-            return "";
-        }
-
-        [KernelFunction]
-        public async Task Type07Async(string input)
-        {
-            await Task.Delay(0);
-        }
-
-        [KernelFunction]
-        public async Task Type08Async()
-        {
-            await Task.Delay(0);
-        }
-
-        [KernelFunction]
-        public async ValueTask ReturnsValueTaskAsync()
-        {
-            await Task.Delay(0);
-        }
-
-        [KernelFunction]
-        public async ValueTask<string> ReturnsValueTaskStringAsync()
-        {
-            await Task.Delay(0);
-            return "hello world";
-        }
-
-        [KernelFunction]
-        public FunctionResult ReturnsFunctionResult()
-        {
-            return new FunctionResult(s_nopFunction, "fake-result", CultureInfo.InvariantCulture);
-        }
-
-        [KernelFunction]
-        public async Task<FunctionResult> ReturnsTaskFunctionResultAsync()
-        {
-            await Task.Delay(0);
-            return new FunctionResult(s_nopFunction, "fake-result", CultureInfo.InvariantCulture);
-        }
-
-        [KernelFunction]
-        public async ValueTask<FunctionResult> ReturnsValueTaskFunctionResultAsync()
-        {
-            await Task.Delay(0);
-            return new FunctionResult(s_nopFunction, "fake-result", CultureInfo.InvariantCulture);
-        }
-
-        [KernelFunction]
-        public string WithPrimitives(
-            byte a1,
-            byte? b1,
-            sbyte c1,
-            sbyte? d1,
-            short e1,
-            short? f1,
-            ushort g1,
-            ushort? h1,
-            int i1,
-            int? j1,
-            uint k1,
-            uint? l1,
-            long m1,
-            long? n1,
-            ulong o1,
-            ulong? p1,
-            float q1,
-            float? r1,
-            double s1,
-            double? t1,
-            decimal u1,
-            decimal? v1,
-            char w1,
-            char? x1,
-            bool y1,
-            bool? z1,
-            DateTime a2,
-            DateTime? b2,
-            DateTimeOffset c2,
-            DateTimeOffset? d2,
-            TimeSpan e2,
-            TimeSpan? f2,
-            Guid g2,
-            Guid? h2,
-            DayOfWeek i2,
-            DayOfWeek? j2,
-            Uri k2,
-            string l2)
-        {
-            return string.Empty;
-        }
-
-        [KernelFunction, CustomAttribute]
-        public void FunctionWithCustomAttribute()
-        {
-        }
-    }
-
-    private sealed class GenericPlugin<T>
-    {
-        [KernelFunction]
-        public int GetValue1(int input) => input;
-
-        [KernelFunction]
-        public T GetValue2(T input) => input;
-
-        [KernelFunction]
-        public Task<T> GetValue3Async(T input) => Task.FromResult(input);
-    }
-
-    [AttributeUsage(AttributeTargets.Method)]
-    private sealed class CustomAttribute : Attribute
+private sealed class LocalExamplePlugin
+{
+    [KernelFunction]
+    public void Type01()
     {
     }
+
+    [KernelFunction]
+    public string Type02()
+    {
+        return "";
+    }
+
+    [KernelFunction]
+    public string? Type02Nullable()
+    {
+        return null;
+    }
+
+    [KernelFunction]
+    public async Task<string> Type03Async()
+    {
+        await Task.Delay(0);
+        return "";
+    }
+
+    [KernelFunction]
+    public async Task<string?> Type03NullableAsync()
+    {
+        await Task.Delay(0);
+        return null;
+    }
+
+    [KernelFunction]
+    public void Type04(string input)
+    {
+    }
+
+    [KernelFunction]
+    public void Type04Nullable(string? input)
+    {
+    }
+
+    [KernelFunction]
+    public void Type04Optional([Optional] string input)
+    {
+    }
+
+    [KernelFunction]
+    public string Type05(string input)
+    {
+        return "";
+    }
+
+    [KernelFunction]
+    private string? Type05Nullable(string? input = null)
+    {
+        return "";
+    }
+
+    [KernelFunction]
+    internal string? Type05EmptyDefault(string? input = "")
+    {
+        return "";
+    }
+
+    [KernelFunction]
+    public string? Type05DefaultProvided(string? input = "someDefault")
+    {
+        return "";
+    }
+
+    [KernelFunction]
+    public async Task<string> Type06Async(string input)
+    {
+        await Task.Delay(0);
+        return "";
+    }
+
+    [KernelFunction]
+    public async Task<string?> Type06NullableAsync(string? input)
+    {
+        await Task.Delay(0);
+        return "";
+    }
+
+    [KernelFunction]
+    public async Task Type07Async(string input)
+    {
+        await Task.Delay(0);
+    }
+
+    [KernelFunction]
+    public async Task Type08Async()
+    {
+        await Task.Delay(0);
+    }
+
+    [KernelFunction]
+    public async ValueTask ReturnsValueTaskAsync()
+    {
+        await Task.Delay(0);
+    }
+
+    [KernelFunction]
+    public async ValueTask<string> ReturnsValueTaskStringAsync()
+    {
+        await Task.Delay(0);
+        return "hello world";
+    }
+
+    [KernelFunction]
+    public FunctionResult ReturnsFunctionResult()
+    {
+        return new FunctionResult(s_nopFunction, "fake-result", CultureInfo.InvariantCulture);
+    }
+
+    [KernelFunction]
+    public async Task<FunctionResult> ReturnsTaskFunctionResultAsync()
+    {
+        await Task.Delay(0);
+        return new FunctionResult(s_nopFunction, "fake-result", CultureInfo.InvariantCulture);
+    }
+
+    [KernelFunction]
+    public async ValueTask<FunctionResult> ReturnsValueTaskFunctionResultAsync()
+    {
+        await Task.Delay(0);
+        return new FunctionResult(s_nopFunction, "fake-result", CultureInfo.InvariantCulture);
+    }
+
+    [KernelFunction]
+    public string WithPrimitives(
+        byte a1,
+        byte? b1,
+        sbyte c1,
+        sbyte? d1,
+        short e1,
+        short? f1,
+        ushort g1,
+        ushort? h1,
+        int i1,
+        int? j1,
+        uint k1,
+        uint? l1,
+        long m1,
+        long? n1,
+        ulong o1,
+        ulong? p1,
+        float q1,
+        float? r1,
+        double s1,
+        double? t1,
+        decimal u1,
+        decimal? v1,
+        char w1,
+        char? x1,
+        bool y1,
+        bool? z1,
+        DateTime a2,
+        DateTime? b2,
+        DateTimeOffset c2,
+        DateTimeOffset? d2,
+        TimeSpan e2,
+        TimeSpan? f2,
+        Guid g2,
+        Guid? h2,
+        DayOfWeek i2,
+        DayOfWeek? j2,
+        Uri k2,
+        string l2)
+    {
+        return string.Empty;
+    }
+
+    [KernelFunction, CustomAttribute]
+    public void FunctionWithCustomAttribute()
+    {
+    }
+}
+
+private sealed class GenericPlugin<T>
+{
+    [KernelFunction]
+    public int GetValue1(int input) => input;
+
+    [KernelFunction]
+    public T GetValue2(T input) => input;
+
+    [KernelFunction]
+    public Task<T> GetValue3Async(T input) => Task.FromResult(input);
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+private sealed class CustomAttribute : Attribute
+{
+}
 }
